ea75ccec1a902fc0ae69cc05c605225b6cd6b885
Matt_21 <m.lopes.1795@gmail.com>
2016-10-04 16:02:36 -0300

    Generating GETs

--- EspacoAluno/archives/db_manager.php
+++ EspacoAluno/archives/db_manager.php
@@ -12,7 +12,7 @@ class DBConnector extends PDO {
     public function __construct(){
         $this->engine = 'mysql';
         $this->host = 'localhost';
-        $this->database = 'teste';
+        $this->database = 'mydb';
         $this->user = 'root';
         $this->pass = '';
         $dns = $this->engine.':dbname='.$this->database.";host=".$this->host;
